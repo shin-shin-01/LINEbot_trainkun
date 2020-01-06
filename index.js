5c5edec8983c26a840b5b89455dc3a5f5915b1ca
Kaze Shindo <shindo@f.ait.kyushu-u.ac.jp>
2020-01-07 03:50:40 +0900

    Improve

--- index.js
+++ index.js
@@ -104,15 +104,15 @@ async function getTrainTime( departure, arrival, line,  updown, time){
 
         if(list.indexOf("カレンダー時以降") !== -1){
 
-          if(TIME(time, list.split("降")[1].trim()){
-          replyMessage.push(list.split("降")[1].trim());
-          count++;
+          if( TIME (time, list.split("降")[1].trim() ) ){
+            replyMessage.push(list.split("降")[1].trim());
+            count++;
           }
           start_flag = true;
 
         }else{
           if(start_flag){
-            if(TIME(time, list.trim())){
+            if( TIME (time, list.trim() ) ){
               replyMessage.push(list.trim());
               count++;
             }
