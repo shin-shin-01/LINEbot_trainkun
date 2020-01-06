94d928c4d6f51fb09238e568678946ebe3c5aa58
Kaze Shindo <shindo@f.ait.kyushu-u.ac.jp>
2020-01-06 17:19:25 +0900

    Improve

---
+++ index.js
@@ -0,0 +1,35 @@
+// ---------------------------------------------------
+// モジュールのインポート
+const server = require('express')();
+// Messaging APIのSDKをインポート
+const line = require('@line/bot-sdk');
+// Scraping
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;  // SSL証明書検証エラーを無視する設定
+const cheerio = require('cheerio-httpcli');
+
+// ---------------------------------------------------
+// パラメータ設定
+const line_config = {
+  channelAccessToken: process.env.LINE_ACCESS_TOKEN,
+  channelSecret: process.env.LINE_CHANNEL_SECRET
+};
+
+// ---------------------------------------------------
+// Webサーバ設定
+server.listen(process.env.PORT || 3000);
+// APIコールのためのクライアントインスタンスを作成
+const bot = new line.Client(line_config);
+
+// ----------------------------------------------------
+// ルーター設定
+server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
+  // 先行してLINE側にステータスコード200でレスポンスする
+  res.sendStatus(200);
+
+  Promise.all(req.body.events.map(handleEvent));
+});
+
+// ----------------------------------------------------
+async function handleEvent(event){
+
+}
