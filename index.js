4742180f1f692632a29cd4d9847fcdffe603adcb
Kaze Shindo <shindo@f.ait.kyushu-u.ac.jp>
2020-01-07 14:40:01 +0900

    Improve

--- index.js
+++ index.js
@@ -52,6 +52,15 @@ async function handleEvent(event){
            type: "text",
            text: response
         };
+      } else if(event.postback.data == "天神"){
+
+        var res = await getTrainTime("00006431", "00009453", "00000836", "1", event.postback.params.time, "天神 → 九大学研都市駅");
+        var response = res.join('\n');
+
+         responsemsg = {
+             type: "text",
+             text: response
+          };
       } else if(event.postback.data == "博多"){
 
       var res = await getTrainTime("00007420", "00009453", "00000836", "1", event.postback.params.time, "博多 → 九大学研都市駅");
@@ -86,7 +95,13 @@ async function handleEvent(event){
             },
             {
             type:"datetimepicker",
-            label:"博多駅発",
+            label:"天神発",
+            data:"天神",
+            mode:"time"
+            },
+            {
+            type:"datetimepicker",
+            label:"博多発",
             data:"博多",
             mode:"time"
             }
