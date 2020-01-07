900c110832ba73c9b5ede3b319addd8368197840
Kaze Shindo <shindo@f.ait.kyushu-u.ac.jp>
2020-01-07 10:19:58 +0900

    Improve

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ async function handleEvent(event){
   if(event.type === 'postback'){
     if(event.postback.data == "九大学研都市"){
 
-      var res = await getTrainTime("00009453", "00007420", "00000016", "0", event.postback.params.time);
+      var res = await getTrainTime("00009453", "00007420", "00000016", "0", event.postback.params.time, "九大学研都市駅 => 博多");
       var response = res.join('\n');
 
        responsemsg = {
@@ -45,7 +45,7 @@ async function handleEvent(event){
 
     } else if(event.postback.data == "博多"){
 
-      var res = await getTrainTime("00007420", "00009453", "00000836", "1", event.postback.params.time);
+      var res = await getTrainTime("00007420", "00009453", "00000836", "1", event.postback.params.time, "博多 => 九大学研都市駅");
       var response = res.join('\n');
 
        responsemsg = {
@@ -84,7 +84,7 @@ async function handleEvent(event){
 } // function-end
 
 
-async function getTrainTime( departure, arrival, line,  updown, time){
+async function getTrainTime( departure, arrival, line,  updown, time, name){
 
   //https://www.navitime.co.jp/diagram/depArrTimeList?departure=00009453&arrival=00007420&line=00000016&updown=0&hour=4&date=2020-01-09
   const cheerioObject = await cheerio.fetch('https://www.navitime.co.jp/diagram/depArrTimeList',{departure:departure,arrival:arrival,line:line,updown:updown});
@@ -96,6 +96,7 @@ async function getTrainTime( departure, arrival, line,  updown, time){
 
   var start_flag = false;
   var count = 0
+  list.push(name);
 
   lists.forEach((list) => {
     if(count >= 5){
@@ -127,7 +128,7 @@ async function getTrainTime( departure, arrival, line,  updown, time){
         }
     }
   });
-  replyMessage.pop();
+  // replyMessage.pop();
   // console.log(time);
   return replyMessage;
 }
