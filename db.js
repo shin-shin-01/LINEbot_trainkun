4f951e3ebf9229dc497e42f01e300fdaaee6b0b8
Kaze Shindo <shindo@f.ait.kyushu-u.ac.jp>
2020-07-21 01:24:12 +0900

    add user register function

---
+++ db.js
@@ -0,0 +1,97 @@
+
+// ---------------------------------------------------
+const { Client } = require('pg');
+const { response } = require('express');
+// BusData Json
+const busdata = require('./bus.json');
+
+// パラメータ設定
+require('dotenv').config({debug: true});
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+});
+
+client.connect();
+
+// ---------------------------------------------------
+class DB {
+    async registerDB( userID, idx, where) {
+        /*
+        DBに登録する作業 
+        */
+       const update_query = {
+           text: `UPDATE userdb SET bus${idx}=($1) where userid=($2)`,
+           values: [where, userID]
+       }
+       client.query(update_query);
+    }
+    
+    async getUserDB(userID) {
+        /*
+        ユーザ情報を取得する作業 
+        */
+        const get_query = {
+            text: 'select * from userdb where userid=($1)',
+            values: [userID]
+        }
+        var result = await client.query(get_query)
+            .then(res => {
+                return res.rows[0]
+            });
+        
+        // ユーザ情報がない場合の初期値
+        if (!result){
+            const add_query = {
+                text: 'INSERT INTO userdb(userid, bus1, bus2, bus3, bus4) VALUES($1, $2, $3, $4, $5)',
+                values: [userID, '産学連携', '九大学研', '中央図書', '九大学研'],
+                }
+                
+            client.query(add_query);
+
+            return {
+                bus1: '産学連携交流センター',
+                bus2: '九大学研都市',
+                bus3: '中央図書館',
+                bus4: '九大学研都市'
+              }
+        } else {
+            return result;
+        }
+    }
+
+    async SelectBus(num) {
+        /*
+        バス停を選択する作業 
+        */
+        let responsemsg = {
+            "type": "text",
+            "text": `Select ${num} Bus Stop`,
+            "quickReply": {
+              "items": []
+            }
+          }
+
+        for (var key in busdata) {
+            if (key.indexOf(' → ') !== -1){
+                break;
+            }
+            responsemsg["quickReply"]["items"].push({
+                "type": "action",
+                "action": {
+                    "type":"postback",
+                    "label": key,
+                    "data": 'R'+num+key
+                }
+            })
+        }
+        return responsemsg;
+    }
+
+
+}
+
+
+
+module.exports = DB;
--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const line = require('@line/bot-sdk');
 // Scraping function
 const scraping = require('./scraping.js');
 const scrape = new scraping();
+// DB register
+const db = require('./db.js');
+const db_func = new db();
+// BusData Json
+const busdata = require('./bus.json');
+const e = require('express');
 
 // ---------------------------------------------------
 // パラメータ設定
@@ -64,57 +70,71 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
 
 async function handleEvent(event){
   console.log(event);
+  // ユーザ情報
+  let userid = event.source.userId;
+  var user_bus = await db_func.getUserDB(userid);
 
   if(event.type === 'postback'){
-    if(event.postback.data == "九大学研都市(博多)"){
-      var code = ["train", "00009453", "00007420", "00000016", "0", "九大学研都市駅 → 博多"];
+    // 登録作業でのPOSTBACK
+    if (event.postback.data.slice(0, 1) == 'R') {
+      if((event.postback.data).length == 2){
+        responsemsg = await db_func.SelectBus(event.postback.data.slice(1, 2));
+      } else {
+        await db_func.registerDB(userid, event.postback.data.slice(1, 2), event.postback.data.slice(2));
+        // TODO: 確認作業を簡単に表示しよう
+        responsemsg = {
+          type: "text",
+          text: "現在の登録をばすくんで確認してね（改良中）"
+        }
+      }
+    } else {
 
-    } else if(event.postback.data == "九大学研都市(天神)"){
-      var code = ["train", "00009453", "00006431", "00000016", "0", "九大学研都市駅 → 天神"];
+      if(event.postback.data == "九大学研都市(博多)"){
+        var code = ["train", "00009453", "00007420", "00000016", "0", "九大学研都市駅 → 博多"];
 
-    } else if(event.postback.data == "天神"){
-      var code = ["train", "00006431", "00009453", "00000836", "1", "天神 → 九大学研都市駅"];
+      } else if(event.postback.data == "九大学研都市(天神)"){
+        var code = ["train", "00009453", "00006431", "00000016", "0", "九大学研都市駅 → 天神"];
 
-    } else if(event.postback.data == "博多"){
-      var code = ["train", "00007420", "00009453", "00000836", "1", "博多 → 九大学研都市駅"];
+      } else if(event.postback.data == "天神"){
+        var code = ["train", "00006431", "00009453", "00000836", "1", "天神 → 九大学研都市駅"];
 
-    } else if(event.postback.data == "九大学研都市(産学連携)"){
-      var code = ["bus", "00291944", "00087909", "九大学研都市 → 産学連携"];
-      var root = ["00053907", "00053915"];
+      } else if(event.postback.data == "博多"){
+        var code = ["train", "00007420", "00009453", "00000836", "1", "博多 → 九大学研都市駅"];
 
-    } else if(event.postback.data == "産学連携(九大学研都市)"){
-      var code = ["bus", "00087909", "00291944", "産学連携 → 九大学研都市"];
-      var root = ["00053907", "00053915", "00053917"];
-      
-    } else if(event.postback.data == "九大学研都市(中央図書館)"){
-      var code = ["bus", "00291944", "00291995", "九大学研都市 → 中央図書館"];
-      var root = ["00053907", "00053914", "00053915"];
+        
+      } else if(event.postback.data.indexOf(' → ') !== -1){
+        // バスデータ
+        let st_en = event.postback.data.split(' → ');
+        var code = ["bus", busdata[st_en[0]], busdata[st_en[1]], event.postback.data];
+        var root = busdata[event.postback.data];
+      }
 
-    } else if(event.postback.data == "中央図書館(九大学研都市)"){
-      var code = ["bus", "00291995",　"00291944", "中央図書館 → 九大学研都市"];
-      var root = ["00053907", "00053914", "00053915"];
-
-    }
-
-    if (code[0] == "train") {
-      var res = await scrape.getTrainTime(code[1], code[2], code[3], code[4], event.postback.params.time, code[5]);
-      var responsemsg = {
-        type: "text",
-        text: res.join('\n')
-      };
-
-    } else if (code[0] == "bus") {
-      var responsemsg = [];
-
-      for (var idx in root) {
-        var res = await scrape.getBusTime(code[1], code[2], root[idx], event.postback.params.time, code[3]);
-        responsemsg.push({
+      if (code[0] == "train") {
+        var res = await scrape.getTrainTime(code[1], code[2], code[3], code[4], event.postback.params.time, code[5]);
+        var responsemsg = {
           type: "text",
           text: res.join('\n')
-        });
+        };
+
+      } else if (code[0] == "bus") {
+        var responsemsg = [];
+
+        if ((!root)||(root[0] == "error")) {
+          responsemsg.push({
+            type: "text",
+            text: "該当する時刻表がありません。追加を待ってね"
+          });
+        } else {
+          for (var idx in root) {
+            var res = await scrape.getBusTime(code[1], code[2], root[idx], event.postback.params.time, code[3]);
+            responsemsg.push({
+              type: "text",
+              text: res.join('\n')
+            });
+          }
+        }
       }
     }
-
   // postback else
   } else if (event.message.text.indexOf("でんしゃくん") !== -1){
 
@@ -166,33 +186,52 @@ async function handleEvent(event){
         actions:[
           {
           type:"datetimepicker",
-          label:"九大学研 → 産学連携",
-          data:"九大学研都市(産学連携)",
+          label: user_bus.bus1.slice(0,4) + " → " + user_bus.bus2.slice(0,4),
+          data: user_bus.bus1.slice(0,4) + " → " + user_bus.bus2.slice(0,4),
           mode:"time"
           },
           {
           type:"datetimepicker",
-          label:"産学連携 → 九大学研",
-          data:"産学連携(九大学研都市)",
+          label: user_bus.bus2.slice(0,4) + " → " + user_bus.bus1.slice(0,4),
+          data: user_bus.bus2.slice(0,4) + " → " + user_bus.bus1.slice(0,4),
           mode:"time"
           },
           {
           type:"datetimepicker",
-          label:"九大学研 → 中央図書館",
-          data:"九大学研都市(中央図書館)",
+          label: user_bus.bus3.slice(0,4) + " → " + user_bus.bus4.slice(0,4),
+          data: user_bus.bus3.slice(0,4) + " → " + user_bus.bus4.slice(0,4),
           mode:"time"
           },
           {
           type:"datetimepicker",
-          label:"中央図書館 → 九大学研",
-          data:"中央図書館(九大学研都市)",
+          label: user_bus.bus4.slice(0,4) + " → " + user_bus.bus3.slice(0,4),
+          data: user_bus.bus4.slice(0,4) + " → " + user_bus.bus3.slice(0,4),
           mode:"time"
           }
           ]
       }// template
     };//respose
 
-  } else{
+  } else if (event.message.text.indexOf("登録") !== -1) {
+     var responsemsg = {
+        "type": "text",
+        "text": `変更するバス停を選択してね`,
+        "quickReply": {
+          "items": []
+        }
+      }
+      for (var i=1; i<5; i++) {
+        responsemsg["quickReply"]["items"].push({
+          "type": "action",
+          "action": {
+              "type":"postback",
+              "label": user_bus[`bus${i}`],
+              "data": 'R'+String(i)
+          }
+        })
+      }
+
+  } else {
     responsemsg = {
       type: "text",
       text: "メニューから選択してください"
